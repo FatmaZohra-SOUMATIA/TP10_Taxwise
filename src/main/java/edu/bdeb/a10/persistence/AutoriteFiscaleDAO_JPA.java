79633c17addf797d177aa0004badb712bda258bd
6284909 <6284909@bdeb.qc.ca>
2024-07-13 12:44:32 -0400

    ajout d un tarchesRevenuDAO JPA

--- src/main/java/edu/bdeb/a10/persistence/AutoriteFiscaleDAO_JPA.java
+++ src/main/java/edu/bdeb/a10/persistence/AutoriteFiscaleDAO_JPA.java
@@ -1,22 +1,21 @@
 package edu.bdeb.a10.persistence;
 
-import edu.bdeb.a10.model.TranchesRevenu;
-import edu.bdeb.a10.view.TaxwiseVue;
+import edu.bdeb.a10.model.AutoriteFiscale;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import org.mariadb.jdbc.export.HaMode;
 import org.mariadb.jdbc.Driver;
+import org.mariadb.jdbc.export.HaMode;
 
 import java.util.List;
 
-public class TranchesRevenuDAO_JPA {
+public class AutoriteFiscaleDAO_JPA {
     Taxwise_PersistenceUnitinfo taxwise_pui = new Taxwise_PersistenceUnitinfo();
     EntityManagerFactory emf = new HibernatePersistenceProvider()
             .createEntityFactory(taxwise_pui, new HaMode());
     EntityManager em = emf.createEntityManager().createEntityM();
 
 
-    public List<TranchesRevenu> trouverTranchesParAutorite(String autorite_id) {
-        return this.em.find(TranchesRevenu.class, autorite_id);
+    public AutoriteFiscale trouverTranchesParAutorite(String autorite_id) {
+        return this.em.find(AutoriteFiscale.class, autorite_id);
     }
 }
---
+++ src/main/java/edu/bdeb/a10/persistence/Taxwise_PersistenceUnitinfo.java
@@ -0,0 +1,117 @@
+package edu.bdeb.a10.persistence;
+
+import jakarta.persistence.SharedCacheMode;
+import jakarta.persistence.ValidationMode;
+import jakarta.persistence.spi.ClassTransformer;
+import jakarta.persistence.spi.PersistenceUnitInfo;
+import jakarta.persistence.spi.PersistenceUnitTransactionType;
+
+import javax.sql.DataSource;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class Taxwise_PersistenceUnitinfo implements PersistenceUnitInfo {
+
+    @Override
+    public String getPersistenceUnitName() {
+        return "Taxwise_PersistenceUnitinfo";
+    }
+
+    @Override
+    public String getPersistenceProviderClassName() {
+        return "org.hibernate.jpa.HibernatePersistenceProvider";
+    }
+
+    @Override
+    public PersistenceUnitTransactionType getTransactionType() {
+        return PersistenceUnitTransactionType.RESOURCE_LOCAL;
+    }
+
+    @Override
+    public DataSource getJtaDataSource() {
+        return null;
+    }
+
+    @Override
+    public DataSource getNonJtaDataSource() {
+        return null;
+    }
+
+    @Override
+    public List<String> getMappingFileNames() {
+        return List.of();
+    }
+
+    @Override
+    public List<URL> getJarFileUrls() {
+        return List.of();
+    }
+
+    @Override
+    public URL getPersistenceUnitRootUrl() {
+        return null;
+    }
+
+    @Override
+    public List<String> getManagedClassNames() {
+        List<String> managedClassNames = new ArrayList<>();
+        managedClassNames.add("TP10_Taxwise.model.AutoriteFiscale");
+        managedClassNames.add("TP10_Taxwise.model.TranchesRevenu");
+        return managedClassNames;
+    }
+
+    @Override
+    public boolean excludeUnlistedClasses() {
+        return false;
+    }
+
+    @Override
+    public SharedCacheMode getSharedCacheMode() {
+        return null;
+    }
+
+    @Override
+    public ValidationMode getValidationMode() {
+        return null;
+    }
+
+    @Override
+    public Properties getProperties() {
+        Properties props = new Properties();
+        props.setProperty("javax.persistence.jdbc.url", "jdbc:mariadb://mysql-fatmaa10.alwaysdata.net:3306");
+        props.setProperty("javax.persistence.jdbc.user", "fatmaa10_usertax");
+        props.setProperty("javax.persistence.jdbc.password", "password_userTaxe");
+        props.setProperty("javax.persistence.jdbc.driver", "org.mariadb.jdbc.AutoriteFiscale");
+        props.setProperty("javax.persistence.jdbc.driver", "org.mariadb.jdbc.TranchesRevenu");
+
+
+        props.setProperty("hibernate.format_sql", "false");
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.mariadb.MariaDbDialect");
+        props.setProperty("hibernate.show_sql", "tr");
+        props.setProperty("hibernate.use_sql_comments", "false");
+        props.setProperty("hibernate.ejb.interceptors", "org.hibernate.ejb.interceptors.EmptyInterceptor");
+        return props;
+    }
+
+    @Override
+    public String getPersistenceXMLSchemaVersion() {
+        return "";
+    }
+
+    @Override
+    public ClassLoader getClassLoader() {
+        return null;
+    }
+
+    @Override
+    public void addTransformer(ClassTransformer classTransformer) {
+
+    }
+
+    @Override
+    public ClassLoader getNewTempClassLoader() {
+        return null;
+    }
+}
